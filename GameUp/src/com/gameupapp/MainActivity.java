0ea81a3ffb663f637ade25c3e2d602842b03c3ae
erinkidd01 <erinkidd01@gmail.com>
2014-02-28 17:26:07 -0800

    So... Erin was the only user for the app

--- GameUp/src/com/gameupapp/MainActivity.java
+++ GameUp/src/com/gameupapp/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends Activity implements OnGameClicked {
 		
 		// Logging into Facebook if active previously logged in
 		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
-		
 		if (loggedIn) {
 			Log.d("facebook", "logged in - restoring session");
 			Session session = Session.getActiveSession();
