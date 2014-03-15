8fd71dcd7ffe8edb9d5b4bde550e28545215ab32
erinkidd01 <erinkidd01@gmail.com>
2014-03-15 03:24:15 -0700

    Don't know what I changed

--- GameUp/src/com/gameupapp/MainActivity.java
+++ GameUp/src/com/gameupapp/MainActivity.java
@@ -6,6 +6,10 @@ import java.util.List;
 import com.gameupapp.GameFragment.OnGameClicked;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
@@ -13,6 +17,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
@@ -22,6 +27,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
