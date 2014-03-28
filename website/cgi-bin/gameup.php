9383703fbc7cc3da4b3a48b9882f1b7a756ae931
erinkidd01 <erinkidd01@gmail.com>
2014-03-28 00:38:49 -0700

    Added backend for website

---
+++ website/cgi-bin/gameup.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_POST['emailSubmit'])) {
+    $file = fopen('~/Desktop/emaillist.txt','a+');
+    $email = $_POST['email'];
+    $fmail = $email.PHP_EOL;
+    fwrite($file,$fmail);
+    fclose($file); 
+    print_r(error_get_last());
+}
+
+?>
---
+++ website/js/gameup.js
@@ -0,0 +1,19 @@
+/** Submits the email, which is saved in a text file */
+function submit(e) {
+    var email = $("#email").val();
+	var dataObject = { 'email': email,
+                       'emailSubmit': true};
+	
+	// Actually submit the rating to the database
+    $.ajax({
+    	type: 'POST',
+    	url: 'cgi-bin/gameup.php',
+     	data: dataObject,
+     	success: function (msg){
+     		alert('Everything is AWESOME!');
+      	}
+    });
+
+    e.preventDefault();
+	$("#email").val("");
+}
