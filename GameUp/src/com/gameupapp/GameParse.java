084f602e5c38f23030818467cf40319550f0096b
Philip Davis <pdavis@hmc.edu>
2014-04-03 23:04:41 -0700

    More testing-friendly interface.

--- GameUp/src/com/gameupapp/GameParse.java
+++ GameUp/src/com/gameupapp/GameParse.java
@@ -139,13 +139,15 @@ public class GameParse extends ParseObject {
 		return getJSONArray("Users");
 	}
 
+	
 	/**
 	 * TODO Throw error up to UI
+	 * @param user User to add.
 	 * @return True on successful add. False if the player had already joined,
 	 * 				the game is full, or the save fails.
 	 */
-	public boolean addPlayer() {
-		if(checkPlayerJoined()) {
+	public boolean addPlayer(ParseUser user) {
+		if(checkPlayerJoined(user)) {
 			return false;
 		}
 		
@@ -160,17 +162,16 @@ public class GameParse extends ParseObject {
 		if(currentCount < maxCount) {
 			// Update game
 			incrementCurrentPlayerCount();
-			ParseUser currentUser = ParseUser.getCurrentUser();
-			addUnique("Users", currentUser.getObjectId());
+			addUnique("Users", user.getObjectId());
 			
 			// Update user
-			currentUser.addUnique("listOfGames", getObjectId());
+			user.addUnique("listOfGames", getObjectId());
 			try {
 				/** TODO handle possible inconsistent state if one save
 				 * succeeds but the other fails.
 				 */
 				save();
-				currentUser.save();
+				user.save();
 				return true;
 			} catch (ParseException e) {
 				Log.e("addPlayer", "Failed to add player", e);
@@ -182,30 +183,39 @@ public class GameParse extends ParseObject {
 	}
 	
 	/**
+	 * TODO Throw error up to UI
+	 * @return True on successful add. False if the player had already joined,
+	 * 				the game is full, or the save fails.
+	 */
+	public boolean addPlayer() {
+		return addPlayer(ParseUser.getCurrentUser());
+	}
+	
+	/**
 	 * Remove a player from the game
+	 * @param user User to be removed
 	 * @return True if the player was successfully removed. False if the remove 
 	 * 		   		failed or player hadn't joined.
 	 */
-	public boolean removePlayer() {
-		if(checkPlayerJoined()) {
+	public boolean removePlayer(ParseUser user) {
+		if(checkPlayerJoined(user)) {
 			// Update game
-			ParseUser currentUser = ParseUser.getCurrentUser();
 			List<String> currentUserList = new ArrayList<String>();
-			currentUserList.add(currentUser.getObjectId());
+			currentUserList.add(user.getObjectId());
 			removeAll("Users", currentUserList);
 			decrementCurrentPlayerCount();
 			
 			// Update user
 			List<String> game = new ArrayList<String>();
 			game.add(getObjectId());
-			currentUser.removeAll("listOofGames", game);
+			user.removeAll("listOofGames", game);
 			
 			try {
 				/** TODO handle possible inconsistent state if one save
 				 * succeeds but the other fails.
 				 */
 				save();
-				currentUser.save();
+				user.save();
 				return true;
 			} catch (ParseException e) {
 				Log.e("removePlayer", "Failed to fuly remove player", e);
@@ -217,23 +227,31 @@ public class GameParse extends ParseObject {
 	}
 	
 	/**
+	 * Remove a player from the game
+	 * @return True if the player was successfully removed. False if the remove 
+	 * 		   		failed or player hadn't joined.
+	 */
+	public boolean removePlayer() {
+		return removePlayer(ParseUser.getCurrentUser());
+	}
+	
+	/**
 	 * Checks if the current user has already joined a game
+	 * @param user User to check for
 	 * @return true if the user has joined. False otherwise (including on error).
 	 */
-	public boolean checkPlayerJoined() { 
+	public boolean checkPlayerJoined(ParseUser user) { 
 		JSONArray joinedPlayers = getJSONArray("Users");
 		if (joinedPlayers == null) {
 			// We have no players
 			return false;
 		}
 		
-		ParseUser currentUser = ParseUser.getCurrentUser();
-		
 		// Anonymous users can't have joined 
-		if(currentUser == null) {
+		if(user == null) {
 			return false;
 		}
-		String currentUID = currentUser.getObjectId();
+		String currentUID = user.getObjectId();
 		for (int i = 0; i < joinedPlayers.length(); i++) {
 			String candidateUID;
 			try {
@@ -248,6 +266,14 @@ public class GameParse extends ParseObject {
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * Checks if the current user has already joined a game
+	 * @return true if the user has joined. False otherwise (including on error).
+	 */
+	public boolean checkPlayerJoined() { 
+		return checkPlayerJoined(ParseUser.getCurrentUser());
 	}
 	
 	/**
